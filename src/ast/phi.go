5c23eddb56f45c74fe99c2b67bef491db5cac5a3
Patrick Lee <pattyshack101@gmail.com>
2024-12-01 23:14:55 -0800

    split phi out of definition

---
+++ src/ast/phi.go
@@ -0,0 +1,68 @@
+package ast
+
+import (
+	"github.com/pattyshack/gt/parseutil"
+)
+
+// For internal use only
+type Phi struct {
+	instruction
+
+	parseutil.StartEndPos
+
+	Dest *VariableDefinition
+
+	// Value is usually a register reference, but could be constant after
+	// optimization.
+	Srcs map[*Block]Value
+}
+
+var _ Instruction = &Phi{}
+
+func (phi *Phi) Walk(visitor Visitor) {
+	visitor.Enter(phi)
+	phi.Dest.Walk(visitor)
+	for _, src := range phi.Srcs {
+		src.Walk(visitor)
+	}
+	visitor.Exit(phi)
+}
+
+func (phi *Phi) replaceSource(oldVal Value, newVal Value) {
+	replaceCount := 0
+	for block, src := range phi.Srcs {
+		if src == oldVal {
+			phi.Srcs[block] = newVal
+			replaceCount++
+		}
+	}
+
+	if replaceCount != 1 {
+		panic("should never happen")
+	}
+}
+
+func (phi *Phi) Sources() []Value {
+	result := make([]Value, 0, len(phi.Srcs))
+	for _, src := range phi.Srcs {
+		result = append(result, src)
+	}
+	return result
+}
+
+func (phi *Phi) Destination() *VariableDefinition {
+	return phi.Dest
+}
+
+func (phi *Phi) Add(parent *Block, def *VariableDefinition) {
+	ref := def.NewRef(phi.StartEnd())
+	ref.SetParent(phi)
+	phi.Srcs[parent] = ref
+}
+
+func (phi *Phi) Discard() {
+	delete(phi.Parent.Phis, phi.Dest.Name)
+	for _, src := range phi.Srcs {
+		src.Discard()
+	}
+}
